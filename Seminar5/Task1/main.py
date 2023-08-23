c4921aaf3f95c9058c3973dfe9ead76701404f0f
ANDREY LEBEDEV <lebedev88@gmail.com>
2023-08-23 21:01:33 +0300

    Seminar 5 completed

---
+++ Seminar5/Task1/main.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from starlette.responses import JSONResponse
+
+app = FastAPI()
+
+TASKS = []
+
+
+class Task(BaseModel):
+    id_: int
+    title: str
+    description: str
+    status: str
+
+
+@app.get('/tasks/')
+async def all_tasts():
+    return {'tasks': TASKS}
+
+
+@app.post('/task/add')
+async def add_task(task: Task):
+    TASKS.append(task)
+    return {'task': task, 'status': 'added'}
+
+
+@app.put('/task/update/{task_id}')
+async def update_task(task_id: int, task: Task):
+    for t in TASKS:
+        if t.id_ == task_id:
+            t.title = task.title
+            t.description = task.description
+            t.status = task.status
+            return {'task': t, 'status': 'added'}
+        return HTTPException(404, 'Task not found')
+
+
+@app.delete('/task/delete/{task_id}')
+async def delete_task(task_id: int):
+    for t in TASKS:
+        if t.id_ == task_id:
+            TASKS.remove(t)
+            return {'status': 'deleted'}
+    return HTTPException(404, 'Task not found')
